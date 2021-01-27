564fe45cfa4a65e5ff925a07bb9968ff0e7c9fb9
andreiguido <andrei.guido@outlook.com>
2021-01-27 17:08:10 -0300

    Service layer, component registration

--- src/main/java/com/example/couse/repositories/UserRepository.java
+++ src/main/java/com/example/couse/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.couse.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.couse.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
